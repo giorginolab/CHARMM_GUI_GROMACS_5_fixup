be50df4a61100b7b809be8d03dda6b287349412b
Toni G <toni.giorgino@gmail.com>
2015-09-01 17:22:07 +0200

    patch first, manually run

--- fixup5.sh
+++ fixup5.sh
@@ -7,22 +7,15 @@ export GMX_MAXBACKUP=-1
 
 
 
-
-
-echo "STEP 6.1 ------------------------------------------"
 i=1
 perl -w $editfield step6.${i}_equilibration.mdp > step6.${i}_equilibration.gromacs5.mdp <<EOF
 cutoff-scheme = group
 EOF
-gmx grompp -f step6.1_equilibration.gromacs5.mdp -o step6.1_equilibration.tpr -c step5_assembly.box.pdb -p system.top -n index.ndx
-gmx mdrun -deffnm step6.1_equilibration
 
 
 
-for i in 2 3 4 5 6
-do
+for i in `seq 2 6`; do 
     j=$((i-1))
-    echo "step6.${i}_equilibration.mdp ----------------------------------------"
     # From http://md.chem.rug.nl/cgmartini/images/parameters/exampleMDP/martini_v2.x_new-rf.mdp
     perl -w $editfield step6.${i}_equilibration.mdp > step6.${i}_equilibration.gromacs5.mdp <<EOF
 cutoff-scheme            = Verlet
@@ -38,14 +31,8 @@ nstxout = 0
 nstvout = 0
 nstfout = 0
 EOF
-    gmx grompp -f step6.${i}_equilibration.gromacs5.mdp -o step6.${i}_equilibration.tpr -c step6.${j}_equilibration.gro -p system.top -n index.ndx
-    gmx mdrun -deffnm step6.${i}_equilibration
 done
 
-
-
-
-echo "STEP 7 PRODUCTION ----------------------------------------"
 
 perl -w $editfield step7_production.mdp > step7_production.gromacs5.mdp <<EOF
 cutoff-scheme            = Verlet
@@ -61,7 +48,34 @@ nstxout = 0
 nstvout = 0
 nstfout = 0
 EOF
-gmx grompp -f step7_production.gromacs5.mdp -o step7_production.tpr -c step6.6_equilibration.gro -p system.top -n index.ndx
+
+
+
+
+cat <<EOF
+-------------------- FILES HAVE BEEN PATCHED --------------------
+
+Now you may be able to run the following commands, in order:
+
+gmx grompp -f step6.1_equilibration.gromacs5.mdp -o step6.1_equilibration.tpr    -c step5_assembly.box.pdb -p system.top -n index.ndx
+gmx mdrun -deffnm step6.1_equilibration
+
+gmx grompp -f step6.2_equilibration.gromacs5.mdp -o step6.2_equilibration.tpr -c step6.1_equilibration.gro -p system.top -n index.ndx
+gmx mdrun -deffnm step6.2_equilibration
+
+gmx grompp -f step6.3_equilibration.gromacs5.mdp -o step6.3_equilibration.tpr -c step6.2_equilibration.gro -p system.top -n index.ndx
+gmx mdrun -deffnm step6.3_equilibration
+
+gmx grompp -f step6.4_equilibration.gromacs5.mdp -o step6.4_equilibration.tpr -c step6.3_equilibration.gro -p system.top -n index.ndx
+gmx mdrun -deffnm step6.4_equilibration
+
+gmx grompp -f step6.5_equilibration.gromacs5.mdp -o step6.5_equilibration.tpr -c step6.4_equilibration.gro -p system.top -n index.ndx
+gmx mdrun -deffnm step6.5_equilibration
+
+gmx grompp -f step6.6_equilibration.gromacs5.mdp -o step6.6_equilibration.tpr -c step6.5_equilibration.gro -p system.top -n index.ndx
+gmx mdrun -deffnm step6.6_equilibration
+
+gmx grompp -f step7_production.gromacs5.mdp      -o step7_production.tpr      -c step6.6_equilibration.gro -p system.top -n index.ndx
 gmx mdrun -deffnm step7_production
 
-
+EOF
